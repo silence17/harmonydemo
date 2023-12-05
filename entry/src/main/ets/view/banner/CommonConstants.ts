3f2b2986b3ffce321ff09a8729e69085134f6575
huchangjie <huchangjie@live.cn>
2023-12-05 12:05:13 +0800

    feat：；轮播图控件

---
+++ entry/src/main/ets/view/banner/CommonConstants.ts
@@ -0,0 +1,80 @@
+/**
+ * Common constants for all features.
+ */
+export class CommonConstants {
+
+  /**
+   * animation duration of tab content switching.
+   */
+  static readonly DURATION_ADS = 350;
+
+
+  /**
+   * height of carousel title.
+   */
+  static readonly HEIGHT_CAROUSEL_TITLE = 90;
+
+  /**
+   * fontSize of description.
+   */
+  static readonly FONT_SIZE_DESCRIPTION = 12;
+  /**
+   * font size of title.
+   */
+  static readonly FONT_SIZE_TITLE = 20;
+
+  static readonly FONT_WEIGHT_LIGHT = 400;
+  /**
+   * bold font.
+   */
+  static readonly FONT_WEIGHT_BOLD = 700;
+
+  /**
+   * page layout weight.
+   */
+  static readonly LAYOUT_WEIGHT = 1;
+
+  /**
+   * border angle.
+   */
+  static readonly BORDER_RADIUS = 12;
+
+  /**
+   * line height for more.
+   */
+  static readonly LINE_HEIGHT_MORE = 19;
+  /**
+   * rolling duration.
+   */
+  static readonly SWIPER_TIME = 2500;
+  /**
+   * margin of text bottom.
+   */
+  static readonly BOTTOM_TEXT = 4;
+
+  /**
+   * margin of banner top.
+   */
+  static readonly TOP_ADS = 12;
+  /**
+   * margin of banner left.
+   */
+  static readonly ADS_LEFT = 12;
+
+  /*
+   * maximum width.
+   */
+  static readonly FULL_WIDTH = '100%';
+  /**
+   * maximum height.
+   */
+  static readonly FULL_HEIGHT = '100%';
+  /**
+   * width of tab page.
+   */
+  static readonly PAGE_WIDTH = '100%';
+  /**
+   * height of banner.
+   */
+  static readonly HEIGHT_BANNER = '27%';
+}
