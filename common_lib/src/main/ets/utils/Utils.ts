00aea261aeaee76509c0f083e05d6242e7ed360e
huchangjie <huchangjie@live.cn>
2024-03-21 14:25:43 +0800

    feat：自定义容器

--- common_lib/src/main/ets/utils/Utils.ts
+++ common_lib/src/main/ets/utils/Utils.ts
@@ -3,71 +3,65 @@ import display from '@ohos.display';
 import router from '@ohos.router';
 
 
-export interface IConfig{
+export interface IConfig {
   //当前发布环境
-  env:string;
+  env: string;
 }
 
-let AppConfig:IConfig;
+let AppConfig: IConfig;
 
-class Utils{
-
-
-  initConfig(jsonStr:string){
+export class Utils {
+  initConfig(jsonStr: string) {
     AppConfig = JSON.parse(jsonStr) as IConfig;
-    console.log("AppConfig",AppConfig)
+    console.log("AppConfig", AppConfig)
   }
 
   /**
    * 是否是生产环境
    * @returns
    */
-  isProd(){
+  isProd() {
     return AppConfig.env == 'production';
   }
 
 
-  getUserInfoForRequestHeader(){
-    return {
-
-    }
+  getUserInfoForRequestHeader() {
+    return {}
   }
 
-  getWindowSize(){
+  getWindowSize() {
     let dis = display.getDefaultDisplaySync();
     return {
       width: dis.width,
-      height:dis.height
+      height: dis.height
     }
 
   }
 
 
-  showLoading(p?: { [key:string]:any }){
+  static showLoading(p?: { [key: string]: any }) {
     let r = router.getState();
-    if(!p)p={} as MyEventData;
-    p["pathName"] = r.path+r.name;
+    if (!p) p = {} as MyEventData;
+    p["pathName"] = r.path + r.name;
     p["eventName"] = 'showLoading';
     EventBus.emit(p);
   }
 
-  hideLoading(p?:{ [key:string]:any }){
+  static hideLoading(p?: { [key: string]: any }) {
     let r = router.getState();
-    if(!p)p={}as MyEventData;
-    p["pathName"] = r.path+r.name;
+    if (!p) p = {} as MyEventData;
+    p["pathName"] = r.path + r.name;
     p["eventName"] = 'hideLoading';
     EventBus.emit(p);
   }
 
-  toast(p?: { [key:string]:any }){
+  static toast(p?: { [key: string]: any }) {
     let r = router.getState();
-    if(!p)p={} as MyEventData;
-    p["pathName"] = r.path+r.name;
+    if (!p) p = {} as MyEventData;
+    p["pathName"] = r.path + r.name;
     p["eventName"] = 'showToast';
     EventBus.emit(p);
   }
-
-
 }
 
 export default new Utils();
